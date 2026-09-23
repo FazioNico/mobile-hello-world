1c64a4784740f12ad510f58870a6113d540cc6ac
FazioNico <contact@nicolasfazio.ch>
2026-09-23 11:49:32 +0200

    feat: integrate Ionic framework into Angular application
    - Added @ionic/angular and @ionic/angular-toolkit dependencies in package.json.
    - Updated app.config.ts to include provideIonicAngular in the application configuration.
    - Replaced placeholder HTML in app.html with Ionic components for a more streamlined UI.
    - Modified app.ts to use IonApp and IonContent components instead of RouterOutlet.
    - Introduced variables.css for Ionic dark theme support.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { IonApp, IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [IonApp, IonContent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
